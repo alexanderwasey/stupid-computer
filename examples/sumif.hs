ca2d0a001565b45a980840ee6e138fb371ddf683
Alexander Wasey <alex@wasey.net>
2020-11-18 17:56:18 +0000

    Updated examples. Removed (length xs > 0) - not null is faster!

--- examples/sumif.hs
+++ examples/sumif.hs
@@ -1,5 +1,5 @@
 sum :: [Int] -> Int 
-sum xs = if (length xs > 0) then (head xs) + sum (tail xs) else 0
+sum xs = if (not (null xs)) then (head xs) + sum (tail xs) else 0
 
 length :: [Int] -> Int
 length (x:xs) = 1 + (length xs) 
