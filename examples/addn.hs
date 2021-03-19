b6bdcf9852ee0c47807f4cdbc05572dc647f425a
Alexander Wasey <alex@wasey.net>
2021-03-19 22:16:34 +0000

    Added more examples

---
+++ examples/addn.hs
@@ -0,0 +1,5 @@
+addn :: Int -> (Int -> Int)
+addn n = (+) n
+
+--Example of partial function application
+addn 4 5
---
+++ examples/mult.hs
@@ -0,0 +1,4 @@
+mult x y = x * y
+
+--Example of custom ifix operators
+2 `mult` 3
