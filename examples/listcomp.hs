2cd5073d72d4a1af5eb4dd14332c6b685b912318
Alexander Wasey <alex@wasey.net>
2021-11-08 09:20:14 +0000

    Update examples

--- examples/listcomp.hs
+++ examples/listcomp.hs
@@ -8,5 +8,10 @@ square x = x*x
 doublelarge :: [Int] -> [Int]
 doublelarge xs = [x * 2 | x <- (map square xs), x > 3]
 
+take :: Int -> [a] -> [a]
+take 0 _ = []
+take _ [] = []
+take n (x:xs) = x : (take (n-1) xs)
+
 demo = doublelarge [2,4,5]
 demo2 = doublelarge [] 
--- examples/map.hs
+++ examples/map.hs
@@ -5,6 +5,10 @@ map _ [] = []
 square :: Num a => a -> a 
 square x = x*x 
 
+sum :: Num a => [a] -> a
+sum (x:xs) = x + sum xs
+sum [] = 0
+
 --This is an example of using a higher order function (map)
 --This works with `square` supported, or a lambda expression, which is not
 demo = map (\x -> x * x) [1..3]
