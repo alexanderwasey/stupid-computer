591e9322c474a42a4234ae2f5adcfd1cc2761ac9
Alexander Wasey <alex@wasey.net>
2022-01-19 13:36:21 +0000

    Update map example file

--- examples/map.hs
+++ examples/map.hs
@@ -8,7 +8,3 @@ square x = x*x
 sum :: Num a => [a] -> a
 sum (x:xs) = x + sum xs
 sum [] = 0
-
---This is an example of using a higher order function (map)
---This works with `square` supported, or a lambda expression, which is not
-demo = map (\x -> x * x) [1..3]
