8270197d0658a602911137eec3624ac4a5ce4445
Alexander Wasey <alex@wasey.net>
2022-01-25 16:07:38 +0000

    Update listcomp example

--- examples/listcomp.hs
+++ examples/listcomp.hs
@@ -10,8 +10,5 @@ doublelarge xs = [x * 2 | x <- xs, x > 3]
 
 take :: Integer -> [a] -> [a]
 take 0 _ = []
-take _ [] = []
 take n (x:xs) = x : (take (n-1) xs)
-
-demo = doublelarge [2,4,5]
-demo2 = doublelarge [] 
+take _ [] = []
